b962a74b30624b5f11417d93212615f5cba20ff4
Jennny1 <june9282@gmail.com>
2023-08-29 10:18:48 +0900

    first

--- src/main/java/com/example/springbootblog/domain/Article.java
+++ src/main/java/com/example/springbootblog/domain/Article.java
@@ -1,4 +1,18 @@
 package com.example.springbootblog.domain;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content", nullable = false)
+    private String content;
+
 }
